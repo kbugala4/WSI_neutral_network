a7f027cdb203a429b8224ce3684f4c63d116a80e
kucjan <janekkuc00@gmail.com>
2022-12-18 19:58:05 +0100

    Create layer.py

---
+++ src/layer.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Layer(object):
+  
+  def __init__(self, neurons_count, input_size):
+    self.neurons_count = neurons_count
+    self.input_size = input_size
+    self.weights = np.random.rand(neurons_count, input_size) - 0.5
+    self.bias_vector = np.random.rand(neurons_count, 1) - 0.5
